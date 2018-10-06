5279015beda8f5bcbf69150be123916f6b992bd0
boardkeystown <43831063+boardkeystown@users.noreply.github.com>
2018-10-06 12:55:58 -0400

    morse code program
    this also has the release build under the folder release so anyone can use the .exe (well anyone that is on windows )

---
+++ Morse Code L.O.L/Morse Code L.O.L/MyForm.cpp
@@ -0,0 +1,25 @@
+/*
+GitHub: https://github.com/boardkeystown
+Copyright © 2018 boardkeystown
+*/
+#include "MyForm.h"
+#include <Windows.h>
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+//+------------------------------+
+//Closes console window if true
+//Console window is open if false
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+
+	Morse_Code_L_O_L::MyForm form;
+	Application::Run(%form);
+
+}
